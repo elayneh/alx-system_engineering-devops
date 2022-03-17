88603d0b0ca8da40ef251778a76c14876d514303
b <betenatefera@gmail.com>
2022-03-17 14:33:49 -0400

    some change

--- 0x10-python_network_0/0-body_size.sh
+++ 0x10-python_network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that takes a URL, sends to that URL and displays the size of the body of the response
 
-curl -sI GET "$URL" | grep -i "Content-Length"
+curl -sI GET "$1" | grep -i "Content-Length"
